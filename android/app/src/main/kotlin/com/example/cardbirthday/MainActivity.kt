5722acf4889d0d693c5cf5024730aeae1cfc3621
Naira-12 <nairashabanheeb@gmail.com>
2025-02-06 23:22:33 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/cardbirthday/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cardbirthday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFFFFFFF),
+        body: Center(
+            child: Image(
+          image: AssetImage('assets/images/birth.webp'),
+        )),
+      ),
+    );
+  }
+}
